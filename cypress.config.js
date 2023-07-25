b895948c90894e12dff28d7b96630d27c2012e7a
cevik <suleymancevik64@gmail.com>
2023-07-25 13:38:57 +0300

    pom

--- cypress.config.js
+++ cypress.config.js
@@ -33,4 +33,19 @@ package.json file
   },
 
   npm install --save-dev @faker-js/faker
+
+  // Rastgele bir sayı seçmek için 0 ile 3 arasındaki değerleri kullanırız.
+// Math.random() fonksiyonu 0 (dahil) ile 1 (hariç) arasında rastgele bir ondalıklı sayı döndürür.
+// Ardından bu değeri 4 ile çarparız ki 0 (dahil) ile 4 (hariç) arasında bir ondalıklı sayı elde edelim.
+// Math.floor() fonksiyonu bu ondalıklı sayıyı aşağıya yuvarlayarak tam bir tamsayı elde etmemizi sağlar.
+// Böylece 0, 1, 2 veya 3 değerlerinden birini elde ederiz.
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * 4);
+}
+
+// Örnek kullanım:
+const randomNum = getRandomNumber();
+console.log(randomNum);
+
  */
